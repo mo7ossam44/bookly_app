97da1d9f07413d28addc52bf8a44113bb6b4880e
Mohamed Hossam <m.mansi2hossam@gmail>
2025-08-03 19:45:11 +0300

    add get material app widget and divide my project by MVVM design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -9,6 +11,6 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: SplashView());
   }
 }
