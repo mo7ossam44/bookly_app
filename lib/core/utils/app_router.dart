46efacd8af042ebdc2894b89eacd31fbc67780d4
Mohamed Hossam <m.mansi2hossam@gmail>
2025-08-07 07:07:33 +0300

    add new route to our app router class

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,14 +1,17 @@
+import 'package:bookly/features/home/presentation/views/book_detailes_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/home';
-  
+  static const kBookView = '/bookDetails';
+
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => SplashView()),
       GoRoute(path: kHomeView, builder: (context, state) => HomeView()),
+      GoRoute(path: kBookView, builder: (context, state) => BookDetailesView()),
     ],
   );
 }
--- lib/features/home/presentation/views/widgets/custom_sliver_list_item.dart
+++ lib/features/home/presentation/views/widgets/custom_sliver_list_item.dart
@@ -1,49 +1,54 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSliverListItem extends StatelessWidget {
   const CustomSliverListItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      width: MediaQuery.of(context).size.width,
-      height: 160,
-      child: Row(
-        children: [
-          Image.asset(AssetsData.bookPhoto),
-          SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    'Harry Potter and the Globlet of Fire',
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: kPrimaryFont,
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).push(AppRouter.kBookView),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        width: MediaQuery.of(context).size.width,
+        height: 160,
+        child: Row(
+          children: [
+            Image.asset(AssetsData.bookPhoto),
+            SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      'Harry Potter and the Globlet of Fire',
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: kPrimaryFont,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle20.copyWith(
-                    color: Colors.grey.shade500,
+                  Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle20.copyWith(
+                      color: Colors.grey.shade500,
+                    ),
                   ),
-                ),
-                BookRating(),
-              ],
+                  BookRating(),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
