9e7c84709604ab186874aeed9eb8677f7867906d
Mohamed Hossam <m.mansi2hossam@gmail>
2025-08-29 15:06:29 +0300

    add book model

---
+++ lib/features/home/data/models/access_info.dart
@@ -0,0 +1,97 @@
+import 'package:bookly/features/home/data/models/equb.dart';
+import 'package:equatable/equatable.dart';
+
+class AccessInfo extends Equatable {
+  final String? country;
+  final String? viewability;
+  final bool? embeddable;
+  final bool? publicDomain;
+  final String? textToSpeechPermission;
+  final Epub? epub;
+  final Pdf? pdf;
+  final String? webReaderLink;
+  final String? accessViewStatus;
+  final bool? quoteSharingAllowed;
+
+  const AccessInfo({
+    this.country,
+    this.viewability,
+    this.embeddable,
+    this.publicDomain,
+    this.textToSpeechPermission,
+    this.epub,
+    this.pdf,
+    this.webReaderLink,
+    this.accessViewStatus,
+    this.quoteSharingAllowed,
+  });
+
+  factory AccessInfo.fromJson(Map<String, dynamic> json) => AccessInfo(
+        country: json['country'] as String?,
+        viewability: json['viewability'] as String?,
+        embeddable: json['embeddable'] as bool?,
+        publicDomain: json['publicDomain'] as bool?,
+        textToSpeechPermission: json['textToSpeechPermission'] as String?,
+        epub: json['epub'] == null
+            ? null
+            : Epub.fromJson(json['epub'] as Map<String, dynamic>),
+        pdf: json['pdf'] == null
+            ? null
+            : Pdf.fromJson(json['pdf'] as Map<String, dynamic>),
+        webReaderLink: json['webReaderLink'] as String?,
+        accessViewStatus: json['accessViewStatus'] as String?,
+        quoteSharingAllowed: json['quoteSharingAllowed'] as bool?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'country': country,
+        'viewability': viewability,
+        'embeddable': embeddable,
+        'publicDomain': publicDomain,
+        'textToSpeechPermission': textToSpeechPermission,
+        'epub': epub?.toJson(),
+        'pdf': pdf?.toJson(),
+        'webReaderLink': webReaderLink,
+        'accessViewStatus': accessViewStatus,
+        'quoteSharingAllowed': quoteSharingAllowed,
+      };
+
+  @override
+  List<Object?> get props {
+    return [
+      country,
+      viewability,
+      embeddable,
+      publicDomain,
+      textToSpeechPermission,
+      epub,
+      pdf,
+      webReaderLink,
+      accessViewStatus,
+      quoteSharingAllowed,
+    ];
+  }
+}
+
+
+
+
+class Pdf extends Equatable {
+  final bool? isAvailable;
+  final String? acsTokenLink;
+
+  const Pdf({this.isAvailable, this.acsTokenLink});
+
+  factory Pdf.fromJson(Map<String, dynamic> json) => Pdf(
+        isAvailable: json['isAvailable'] as bool?,
+        acsTokenLink: json['acsTokenLink'] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'isAvailable': isAvailable,
+        'acsTokenLink': acsTokenLink,
+      };
+
+  @override
+  List<Object?> get props => [isAvailable, acsTokenLink];
+}
